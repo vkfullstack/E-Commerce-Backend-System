c6f1da3e1d35872c682851f2d3ba5931843d2b64
vkfullstack <vijaykhetre36@gmail.com>
2026-05-30 17:59:23 +0530

    feat(model): add User entity with JPA mappings

---
+++ E-Commerce/E-Commerce/src/main/java/com/ecommerce/model/User.java
@@ -0,0 +1,31 @@
+package com.ecommerce.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Table(name = "users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String role;
+
+    private String password;
+
+
+}
