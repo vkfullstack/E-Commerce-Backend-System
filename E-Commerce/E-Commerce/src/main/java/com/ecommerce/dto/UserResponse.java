0497c9002824d533c8b84f46eaf78b58ecdd7ef7
vkfullstack <vijaykhetre36@gmail.com>
2026-05-30 18:00:10 +0530

    feat(dto): add UserResponse for API output payload

---
+++ E-Commerce/E-Commerce/src/main/java/com/ecommerce/dto/UserResponse.java
@@ -0,0 +1,17 @@
+package com.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+    private Long id;
+    private  String name;
+    private  String email;
+    private  String role;
+}
